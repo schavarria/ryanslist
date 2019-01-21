57ce818c88a2112792d00945dc20d506e54455de
Sucely Chavarria <schavarria7@yahoo.com>
2019-01-20 17:19:27 -0800

    fixing single category page

--- client/src/components/Singlecat.js
+++ client/src/components/Singlecat.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getsingleCat } from '../actions/ryansActions';
-import { Link } from 'react-router-dom'
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+// import { getsingleCat } from '../actions/ryansActions';
+// import { Link } from 'react-router-dom'
 
-class Scat extends Component {
-  componentDidMount() {
-    getsingleCat(this.props.match.params.id)
-  }
+// class Scat extends Component {
+//   componentDidMount() {
+//     getsingleCat(this.props.match.params.slug, this.props.match.params.id)
+//   }
   
-  render() {
-    return (
-      <div>
-        <h1>Single Category Page</h1>
-        <div>{this.props.posts.map(allposts => (
-              <Link to={`/${allposts.slug}/${allposts.id}`}> <div>{allposts.title}</div> </Link>
-            ))}
-            </div>
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h1>Single Category Page</h1>
+//         <div>{this.props.posts.map(allposts => (
+//               <Link to={`/${allposts.slug}/${allposts.id}`}> <div>{allposts.title}</div> </Link>
+//             ))}
+//             </div>
+//       </div>
+//     )
+//   }
+// }
 
-function mapStateToProps(appState) {
-  console.log(appState)
-  return {
-   scat: appState.ryansReducer.scat
-  }
-}
+// function mapStateToProps(appState) {
+//   console.log(appState)
+//   return {
+//    scat: appState.ryansReducer.scat
+//   }
+// }
 
-export default connect(mapStateToProps)(Scat)
+// export default connect(mapStateToProps)(Scat)
